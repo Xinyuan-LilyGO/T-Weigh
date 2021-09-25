d227c97eb16191c3e233cf9d1bbfcfccb6a354d7
Micky <31160602+LilyGO@users.noreply.github.com>
2021-09-25 17:14:27 +0800

    Update main.cpp

--- example/Lora-Weights/src/main.cpp
+++ example/Lora-Weights/src/main.cpp
@@ -9,57 +9,36 @@
 #include "WiFi.h"
 
 #define GapValue 2208
-#define WIFI_SSID "xinyuan-2"
-#define WIFI_PASS "Xydz202104"
+#define WIFI_SSID ""
+#define WIFI_PASS ""
 
 const long frequency = 868E6; // LoRa Frequency
 static long Weight_Maopi[4];
 bool isWIFItestDone = false;
+bool Monitor = false;
 SPIClass spi;
 SX1262 radio = nullptr;
 HX711 scale;
 U8G2_SSD1306_128X64_NONAME_F_SW_I2C u8g2(U8G2_R0, /* clock=*/IIC_SCL, /* data=*/IIC_SDA, /* reset=*/U8X8_PIN_NONE);
 OneButton button(BTN0, true);
 
+const char channel[4] = {0x0F, 0X0B, 0X09, 0X0D};
+
 void testClient(const char *host, uint16_t port);
 void BTN_task(void *pvParameter);
 void WIFItest_task(void *pvParameter);
 
-void SelectChannel(uint8_t channel)
+void SelectChannel(uint8_t ch)
 {
   pinMode(CDA, OUTPUT);
   pinMode(CDB, OUTPUT);
   pinMode(CDC, OUTPUT);
   pinMode(CDD, OUTPUT);
-  switch (channel)
-  {
-  case 0:
-    digitalWrite(CDA, 1);
-    digitalWrite(CDB, 1);
-    digitalWrite(CDC, 1);
-    digitalWrite(CDD, 1);
-    break;
-  case 1:
-    digitalWrite(CDA, 1);
-    digitalWrite(CDB, 0);
-    digitalWrite(CDC, 1);
-    digitalWrite(CDD, 1);
-    break;
-  case 2:
-    digitalWrite(CDA, 1);
-    digitalWrite(CDB, 0);
-    digitalWrite(CDC, 0);
-    digitalWrite(CDD, 1);
-    break;
-  case 3:
-    digitalWrite(CDA, 1);
-    digitalWrite(CDB, 1);
-    digitalWrite(CDC, 0);
-    digitalWrite(CDD, 1);
-    break;
-  default:
-    break;
-  }
+
+  digitalWrite(CDA, channel[ch] & 0x08);
+  digitalWrite(CDB, channel[ch] & 0x04);
+  digitalWrite(CDC, channel[ch] & 0x02);
+  digitalWrite(CDD, channel[ch] & 0x01);
 }
 
 void CalibrationFunction(void)
@@ -77,6 +56,11 @@ void CalibrationFunction(void)
     count++;
   }
 }
+void SwitchingMonitorFunction(void)
+{
+  Monitor = !Monitor;
+  u8g2.clearDisplay();
+}
 
 void setup()
 {
@@ -92,7 +76,7 @@ void setup()
   int state = radio.begin(868.0);
   if (state == ERR_NONE)
   {
-    Serial.println(F("success!"));
+    Serial.println(F("SX1262 success!"));
   }
   else
   {
@@ -105,6 +89,7 @@ void setup()
   u8g2.setFont(u8g2_font_ncenB08_tr);
 
   button.attachClick(CalibrationFunction);
+  button.attachDoubleClick(SwitchingMonitorFunction);
 
   xTaskCreate(BTN_task, "Task1", 10000, NULL, 2, NULL);
   xTaskCreate(WIFItest_task, "Task2", 10000, NULL, 1, NULL);
@@ -112,45 +97,64 @@ void setup()
 
 void loop()
 {
-  float Weight[4];
-  float TotalWeight;
-  static uint8_t count;
-  char str[25];
 
-  TotalWeight = 0;
-  count = 0;
-  while (count < 4)
+  if (!Monitor)
   {
-    SelectChannel(count);
-    if (scale.is_ready())
+    float Weight[4];
+    float TotalWeight;
+    static uint8_t count;
+    char str[25];
+    TotalWeight = 0;
+    count = 0;
+    while (count < 4)
     {
-      long reading = scale.read();
-      reading = reading - Weight_Maopi[count];
-      Weight[count] = (long)((float)reading / GapValue);
-      if (isWIFItestDone)
-        Serial.printf("[%d] Weight : %0.3fkg\r\n", count, float(Weight[count] / 1000), 3); //串口显示重量
-      TotalWeight += Weight[count];
-      count++;
+      SelectChannel(count);
+      if (scale.is_ready())
+      {
+        long reading = scale.read();
+        reading = reading - Weight_Maopi[count];
+        Weight[count] = (long)((float)reading / GapValue);
+        if (isWIFItestDone)
+          Serial.printf("[%d] Weight : %0.3fkg\r\n", count, float(Weight[count] / 1000), 3); //串口显示重量
+        TotalWeight += Weight[count];
+        count++;
+      }
+    }
+
+    u8g2.clearBuffer();
+    sprintf(str, "[1] Weight : %0.3f kg", float(Weight[0] / 1000));
+    u8g2.drawStr(0, 10, str);
+    sprintf(str, "[2] Weight : %0.3f kg", float(Weight[1] / 1000));
+    u8g2.drawStr(0, 22, str);
+    sprintf(str, "[3] Weight : %0.3f kg", float(Weight[2] / 1000));
+    u8g2.drawStr(0, 34, str);
+    sprintf(str, "[4] Weight : %0.3f kg", float(Weight[3] / 1000));
+    u8g2.drawStr(0, 46, str);
+    sprintf(str, "Total Weight : %0.3f kg", float(TotalWeight / 1000));
+    u8g2.drawStr(0, 58, str);
+    u8g2.sendBuffer();
+
+    if (isWIFItestDone)
+      Serial.println(str);
+    radio.transmit(str);
+  }
+  else
+  {
+    String str, radio_str;
+    int state = radio.receive(radio_str);
+    if (state == ERR_NONE)
+    {
+      u8g2.clearBuffer();
+      str = "Data:";
+      u8g2.drawStr(0, 10, str.c_str());
+      u8g2.drawStr(0, 22, radio_str.c_str());
+      str = "RSSI:" + String(radio.getRSSI()) + " dBm";
+      u8g2.drawStr(0, 34, str.c_str());
+      str = "SNR:" + String(radio.getSNR()) + "  dB";
+      u8g2.drawStr(0, 46, str.c_str());
+      u8g2.sendBuffer();
     }
   }
-
-  u8g2.clearBuffer();
-  sprintf(str, "[1] Weight : %0.3f kg", float(Weight[0] / 1000));
-  u8g2.drawStr(0, 10, str);
-  sprintf(str, "[2] Weight : %0.3f kg", float(Weight[1] / 1000));
-  u8g2.drawStr(0, 22, str);
-  sprintf(str, "[3] Weight : %0.3f kg", float(Weight[2] / 1000));
-  u8g2.drawStr(0, 34, str);
-  sprintf(str, "[4] Weight : %0.3f kg", float(Weight[3] / 1000));
-  u8g2.drawStr(0, 46, str);
-  sprintf(str, "Total Weight : %0.3f kg", float(TotalWeight / 1000));
-  u8g2.drawStr(0, 58, str);
-  u8g2.sendBuffer();
-
-  if (isWIFItestDone)
-    Serial.println(str);
-  radio.transmit(str);
-
   //delay(100);
 }
 
@@ -199,6 +203,7 @@ void WIFItest_task(void *pvParameter)
   }
 
   testClient("baidu.com", 80);
+  Serial.println(F("wifi success!"));
   isWIFItestDone = true;
   vTaskDelete(NULL);
 }
